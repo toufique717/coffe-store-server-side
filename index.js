b0da3b28c8067ba2c444286e395f6862deed1189
toufique717 <toufiquehossain716.com>
2025-05-28 17:13:21 +0600

    jhg

--- index.js
+++ index.js
@@ -115,6 +115,14 @@ async function run() {
      res.send(result);
     })
 
+
+     app.delete('/user/:id',async (req, res) => {
+      const id = req.params.id;
+      const query ={_id: new ObjectId(id)}
+     const result = await  usercollection.deleteOne(query);
+     res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
